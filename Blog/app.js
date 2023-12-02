a3f75bed7e1247db53ff37d935be4091850ca3f8
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2023-12-02 16:04:55 -0100

    Add files via upload

---
+++ Blog/app.js
@@ -0,0 +1,24 @@
+document.getElementById("btn1").addEventListener("click", function() {
+    window.location.href = "Visual Spectacle/index.html";
+});
+
+document.getElementById("btn2").addEventListener("click", function() {
+    window.location.href = "Story/index.html";
+});
+
+document.getElementById("btn3").addEventListener("click", function() {
+    window.location.href = "In-Game Wonders/index.html";
+});
+
+document.getElementById("btn5").addEventListener("click", function() {
+    window.location.href = "Team/index.html";
+});
+
+document.getElementById("btn4").addEventListener("click", function() {
+    window.location.href = "Goals/index.html";
+});
+
+
+
+
+
